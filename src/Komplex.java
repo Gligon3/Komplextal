4101dd60a173c17503dbd8896314e4bbb4930115
Gligon3 <94972508+Gligon3@users.noreply.github.com>
2023-03-14 15:10:35 +0100

    Readme

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class KomplexTal {
+public class Komplex {
+
+    public Komplex(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    double a;
+    double b;
+
+
+    public Komplex add(Komplex b) {
+        return b;
+    }
+
+    public Komplex multiply(Komplex b) {
+        return b;
+    }
+
+    public double arg() {
+        return 0;
+    }
 }
